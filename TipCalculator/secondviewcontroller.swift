19c86b917f0cd5cedc1b29a7ead9d179aeb2f744
Kumaran Kaliamoorthy <kkaliamo@kkaliamo-m01.vmware.com>
2017-03-12 01:28:56 -0800

    first commit

---
+++ TipCalculator/secondviewcontroller.swift
@@ -0,0 +1,13 @@
+//
+//  secondviewcontroller.swift
+//  TipCalculator
+//
+//  Created by Kumaran Kaliamoorthy on 3/7/17.
+//  Copyright © 2017 Home. All rights reserved.
+//
+
+import UIKit
+
+class secondviewcontroller: NSObject {
+
+}
